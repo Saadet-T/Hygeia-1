03f333a233d7a8e462c42e5eaa158848938f84fd
Saadet Elif Tokuoğlu <lzzaplzzap123@gmail.com>
2022-11-10 20:22:13 +0300

    bug fix

--- src/main/java/com/backend/hygeia/resources/AuthController.java
+++ src/main/java/com/backend/hygeia/resources/AuthController.java
@@ -1,4 +1,4 @@
- package com.backend.hygeia.resources;
+package com.backend.hygeia.resources;
 
 import java.io.IOException;
 import java.net.URI;
@@ -41,107 +41,98 @@ import com.backend.hygeia.security.services.UserDetailsImpl;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-  @Autowired
-  AuthenticationManager authenticationManager;
+	@Autowired
+	AuthenticationManager authenticationManager;
 
-  @Autowired
-  UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-  @Autowired
-  RoleRepository roleRepository;
+	@Autowired
+	RoleRepository roleRepository;
 
-  @Autowired
-  PasswordEncoder encoder;
+	@Autowired
+	PasswordEncoder encoder;
 
-  @Autowired
-  JwtUtils jwtUtils;
+	@Autowired
+	JwtUtils jwtUtils;
 
-  @PostMapping("/signin")
-  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-    Authentication authentication = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtUtils.generateJwtToken(authentication);
-    
-    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();    
-    List<String> roles = userDetails.getAuthorities().stream()
-        .map(item -> item.getAuthority())
-        .collect(Collectors.toList());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
 
-    return ResponseEntity.ok(new JwtResponse(jwt, 
-                         userDetails.getId(), 
-                         userDetails.getUsername(), 
-                         userDetails.getEmail(), 
-                         roles));
-  }
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+				.collect(Collectors.toList());
 
-  @PostMapping("/signup")
-  public ResponseEntity<MessageResponse> getLogin(HttpServletRequest request,HttpServletResponse response) throws URISyntaxException, IOException {
-	  String username = request.getParameter("username");
-	  String email = request.getParameter("email");
-      String password = request.getParameter("password");
-      String returnURL = request.getParameter("returnURL");
-	 
-    if (userRepository.existsByUsername(request.getParameter("username"))) {
-      return ResponseEntity
-          .badRequest()
-          .body(new MessageResponse("Error: Username is already taken!"));
-    }
+		return ResponseEntity.ok(
+				new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+	}
 
-    if (userRepository.existsByEmail(request.getParameter("email"))) {
-      return ResponseEntity
-          .badRequest()
-          .body(new MessageResponse("Error: Email is already in use!"));
-    }
+	@PostMapping("/signup")
+	public ResponseEntity<MessageResponse> getLogin(HttpServletRequest request, HttpServletResponse response)
+			throws URISyntaxException, IOException {
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String returnURL = request.getParameter("returnURL");
 
-    // Create new user's account
-    User user = new User(request.getParameter("username"), 
-    		request.getParameter("email"),
-               encoder.encode(request.getParameter("password")),
-               request.getParameter("returnURL"));
-    
-    Set<String> strRoles = new HashSet<>();
-    Set<Role> roles = new HashSet<>();
-    if(request.getParameter("Role")==null) {
-   strRoles.add("user");
-    }
-    else {
-     strRoles.add(request.getParameter("Role"));
-    }
-    
-    if (strRoles == null) {
-      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-          .orElseThrow(() -> new RuntimeException("Error: Role is not found."+ERole.ROLE_USER));
-      roles.add(userRole);
-    } else {
-      strRoles.forEach(role -> {
-        switch (role) {
-        case "admin":
-          Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(adminRole);
+		if (userRepository.existsByUsername(request.getParameter("username"))) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+		}
 
-          break;
-        case "mod":
-          Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(modRole);
+		if (userRepository.existsByEmail(request.getParameter("email"))) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+		}
 
-          break;
-        default:
-          Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(userRole);
-        }
-      });
-    }
+		// Create new user's account
+		User user = new User(request.getParameter("username"), request.getParameter("email"),
+				encoder.encode(request.getParameter("password")), request.getParameter("returnURL"));
 
-    user.setRoles(roles);
-    userRepository.save(user);
-    
-    response.sendRedirect(returnURL);
-    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-  }
+		Set<String> strRoles = new HashSet<>();
+		Set<Role> roles = new HashSet<>();
+		
+		if (request.getParameter("Role") == null) {
+			strRoles.add("user");
+		} else {
+			strRoles.add(request.getParameter("Role"));
+		}
+
+		if (strRoles.isEmpty()) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found." + ERole.ROLE_USER));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+				case "mod":
+					Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(modRole);
+
+					break;
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		response.sendRedirect(returnURL);
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
 }
